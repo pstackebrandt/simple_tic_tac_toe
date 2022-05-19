cc60ce0bd2bc0afe609fa6db62cbbdaa47cc0189
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2022-05-19 20:27:57 +0200

    task Conditional statement is green https://hyperskill.org/learn/step/2735

--- Topics/Conditional statement/The army of units/src/Main.java
+++ Topics/Conditional statement/The army of units/src/Main.java
@@ -3,6 +3,20 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // start coding here
+        final var units = scanner.nextInt();
+        var army = "";
+        if (units < 1) {
+            army = "no army";
+        } else if (units <= 19) {
+            army = "pack";
+        } else if (units <= 249) {
+            army = "throng";
+        } else if (units <= 999) {
+            army = "zounds";
+        } else {
+            army = "legion";
+        }
+
+        System.out.println(army);
     }
 }
