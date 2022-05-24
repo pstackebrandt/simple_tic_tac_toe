33cfebccf9c821e5ac9b09b65d3a7fc670c8d3d2
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2022-05-24 21:12:07 +0200

    task Chocolate (hard) of Conditional statement is red https://hyperskill.org/learn/step/2170

---
+++ Topics/Conditional statement/Chocolate/src/Main.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final var width = scanner.nextInt();
+        final var height = scanner.nextInt();
+        final var expectedPieces = scanner.nextInt();
+
+        boolean dividable = Main.ChocolateBar.dividableIntoBlock(width, height, expectedPieces);
+
+        System.out.println(dividable ? "YES" : "NO");
+    }
+
+    public static class ChocolateBar {
+
+        /**
+         * Returns whether chocolate is dividable into a block of
+         * {@code blockSize} pieces. Width or height of the block must be equal
+         * to width or height of the block.
+         *
+         * @param blockSize Count of pieces of the block.
+         */
+        public static boolean dividableIntoBlock(int width, int height, int blockSize) {
+            boolean dividable = dividableIntoBlock(blockSize, width);
+            if (!dividable) {
+                //noinspection SuspiciousNameCombination
+                dividable = dividableIntoBlock(blockSize, height);
+            }
+            return dividable;
+        }
+
+        /**
+         * Returns whether chocolate is dividable into a block of
+         * {@code blockSize} pieces with 1 side of length {@code blockWidth}.
+         */
+        private static boolean dividableIntoBlock(int expectedBlockSize, int blockWidth) {
+            boolean dividable = false;
+
+            int rows = 1;
+            int pieces = getPiecesCount(blockWidth, rows);
+            while (pieces <= expectedBlockSize) {
+                if (pieces == expectedBlockSize) {
+                    dividable = true;
+                    break;
+                }
+                rows++;
+                pieces = getPiecesCount(blockWidth, rows);
+            }
+            return dividable;
+        }
+
+        /**
+         * Get pieces count of a block.
+         */
+        private static int getPiecesCount(int blockWidth, int blockRows) {
+            return blockWidth * blockRows;
+        }
+    }
+}
+
