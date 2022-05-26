fe817db9c272a303a338fc50de64a5e4463c1852
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2022-05-26 20:42:17 +0200

    Add implementation, tests are green task "The smallest value of "While and do-while loops" https://hyperskill.org/learn/step/2229

--- Topics/The while and do-while loops/The smallest value/src/Main.java
+++ Topics/The while and do-while loops/The smallest value/src/Main.java
@@ -2,7 +2,41 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // start coding here
+        final var scanner = new Scanner(System.in);
+        final var number = scanner.nextLong();
+
+        int iteration = Factorial.getIterationOfFirstBiggerValue(number);
+        System.out.println(iteration);
+    }
+}
+
+/**
+ * Contains code for work with factorial (n!).
+ */
+class Factorial {
+    /**
+     * Get iteration (n) for first factorial value which is bigger than {@param number}.
+     *
+     * @return value n of "n!"
+     */
+    public static int getIterationOfFirstBiggerValue(long number) {
+        var factorial = 1L; // Product
+        var iteration = 1; // Count of multiplications
+
+        while (number >= factorial) {
+            iteration++;
+            factorial = Factorial.getNextFactorial(factorial, iteration);
+        }
+        return iteration;
+    }
+
+    /**
+     * Get next factorial "n!" from {@param factorial} (n-1)! and {@param factor} n
+     *
+     * @param factorial current factorial
+     * @param factor    factor for next factorial
+     */
+    public static long getNextFactorial(long factorial, int factor) {
+        return factorial * factor;
     }
 }
