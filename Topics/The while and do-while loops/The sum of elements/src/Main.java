0ff79393796952c84ca0f76ba458db3d781ef765
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2022-05-26 20:59:50 +0200

    Clean up, tests are green task "Sum of elements" of "While and do-while loops" https://hyperskill.org/learn/step/2227

--- Topics/The while and do-while loops/The sum of elements/src/Main.java
+++ Topics/The while and do-while loops/The sum of elements/src/Main.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
 
 class Main {
+    public static final int STOP_NUMBER = 0;
+
     public static void main(String[] args) {
+
         final var scanner = new Scanner(System.in);
         var sum = 0;
 
         while (true) {
             var summand = scanner.nextInt();
-            if (summand == 0) break;
+            if (summand == STOP_NUMBER) {
+                break;
+            }
             sum += summand;
         }
 
