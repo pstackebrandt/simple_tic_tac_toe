3c391d817488537e0b7923aa062b556bfe9358af
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2022-05-27 17:50:43 +0200

    Add alternative algorithm, tests green task "Sum of integers from a to b" of "for-loop" https://hyperskill.org/learn/step/2187

--- Topics/The for-loop/The sum of integers from a to b/src/Main.java
+++ Topics/The for-loop/The sum of integers from a to b/src/Main.java
@@ -6,18 +6,48 @@ class Main {
         final int startSummand = scanner.nextInt();
         final int endSummand = scanner.nextInt();
 
-        var sum = new Summator().sumAllNumbersAndIncludeBorders(startSummand, endSummand);
-
+        var sum = 0;
+        //noinspection ConstantConditions
+        if (false) {
+            sum = new Summator().sumAllNumbersAndIncludeBorders(startSummand, endSummand);
+        } else {
+            sum = new Summator().sumAllNumbersAndIncludeBordersCalculation(startSummand, endSummand);
+        }
         System.out.println(sum);
     }
 }
 
 class Summator {
+
+    /**
+     * Sum all values between {@param start} and {@param end} thereby include
+     * {@param start} and {@param end}.
+     * <p>
+     * Algorithm: We add all values one after the other.
+     */
     public int sumAllNumbersAndIncludeBorders(int start, int end) {
         int sum = 0;
         for (int i = start; i <= end; i++) {
             sum += i;
         }
         return sum;
+    }
+
+    /**
+     * Sum all values between {@param start} and {@param end} thereby include
+     * {@param start} and {@param end}.
+     *
+     * Algorithm: I get the sum by calculation of the area of the geometrical figure.
+     * If {@param start} and {@param end} are positive numbers bigger than 0 the figure
+     * consists of a rectangular base and a rectangular triangle.
+     */
+    public int sumAllNumbersAndIncludeBordersCalculation(int start, int end) {
+        final int width = end - start + 1;
+        final int baseSum = width * start; // rectangle
+
+        final int triangleHeight = end - start;
+        int triangleSum = width * triangleHeight / 2;
+
+        return baseSum + triangleSum;
     }
 }
