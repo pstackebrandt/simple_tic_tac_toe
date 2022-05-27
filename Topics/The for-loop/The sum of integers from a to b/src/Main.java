43fffe6c4cb6ff015902692b0a4e744d2c80d50c
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2022-05-27 16:53:08 +0200

    Implement, tests green task "Sum of integers from a to b" of "for-loop" https://hyperskill.org/learn/step/2187

--- Topics/The for-loop/The sum of integers from a to b/src/Main.java
+++ Topics/The for-loop/The sum of integers from a to b/src/Main.java
@@ -2,7 +2,22 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // start coding here
+        var scanner = new Scanner(System.in);
+        final int startSummand = scanner.nextInt();
+        final int endSummand = scanner.nextInt();
+
+        var sum = new Summator().sumAllNumbersAndIncludeBorders(startSummand, endSummand);
+
+        System.out.println(sum);
+    }
+}
+
+class Summator {
+    public int sumAllNumbersAndIncludeBorders(int start, int end) {
+        int sum = 0;
+        for (int i = start; i <= end; i++) {
+            sum += i;
+        }
+        return sum;
     }
 }
