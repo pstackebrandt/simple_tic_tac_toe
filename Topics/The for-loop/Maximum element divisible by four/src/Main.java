7b0854048064415bce25569598e3ce8057fad8dd
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2022-05-28 21:07:30 +0200

    Add class Checker, tests green task "Maximum element divisible by four" of "for-loop" https://hyperskill.org/learn/step/2189

--- Topics/The for-loop/Maximum element divisible by four/src/Main.java
+++ Topics/The for-loop/Maximum element divisible by four/src/Main.java
@@ -6,15 +6,24 @@ class Main {
 
         final int numberCount = scanner.nextInt();
 
+        int biggestDividableBy4 = Checker.getBiggestNumberDividableBy4(scanner, numberCount);
+
+        System.out.println(biggestDividableBy4);
+    }
+
+
+}
+
+class Checker {
+    public static int getBiggestNumberDividableBy4(Scanner scanner, int numberCount) {
         int biggestDividableBy4 = 0;
-        for (int i = 0; i < numberCount; i++){
+        for (int i = 0; i < numberCount; i++) {
             int currentNumber = scanner.nextInt();
             if (currentNumber > biggestDividableBy4
-                    && currentNumber % 4 == 0){
+                    && currentNumber % 4 == 0) {
                 biggestDividableBy4 = currentNumber;
             }
         }
-
-        System.out.println(biggestDividableBy4);
+        return biggestDividableBy4;
     }
 }
