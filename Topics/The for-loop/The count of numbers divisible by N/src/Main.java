2766cab7aad00e5d2726da924dd41345638cc34e
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2022-05-28 19:24:05 +0200

    Implemented, tests green task "count of numbers divisible by N" of "for-loop" https://hyperskill.org/learn/step/2743

--- Topics/The for-loop/The count of numbers divisible by N/src/Main.java
+++ Topics/The for-loop/The count of numbers divisible by N/src/Main.java
@@ -2,7 +2,30 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // start coding here
+        final var scanner = new Scanner(System.in);
+
+        final var lowerBorder = scanner.nextInt();
+        final var upperBorder = scanner.nextInt();
+        final var divisor = scanner.nextInt();
+
+        final int divisibleNumbers =
+                Checker.countDivisibleNumbers(lowerBorder, upperBorder, divisor);
+
+        System.out.println(divisibleNumbers);
+    }
+}
+
+class Checker {
+    public static int countDivisibleNumbers(int lowerBorder,
+                                            int upperBorder,
+                                            int divisor) {
+        int count = 0;
+        for (int i = lowerBorder; i <= upperBorder; i++) {
+            if (i % divisor == 0) {
+                count++;
+            }
+        }
+
+        return count;
     }
 }
