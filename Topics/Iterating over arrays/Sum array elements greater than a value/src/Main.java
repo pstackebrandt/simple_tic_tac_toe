ef87d36de032367670a3746738f4db30cb196226
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2022-06-13 19:41:21 +0200

    Add solution, tests green task "Sum array elements greater than a value" of "Iterating over arrays" https://hyperskill.org/learn/step/2134

--- Topics/Iterating over arrays/Sum array elements greater than a value/src/Main.java
+++ Topics/Iterating over arrays/Sum array elements greater than a value/src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        final var scanner = new Scanner(System.in);
+        final var valuesCount = scanner.nextInt();
+        final var values = new int[valuesCount];
+
+        for (var i = 0; i < valuesCount; i++) {
+            values[i] = scanner.nextInt();
+        }
+
+        final int border = scanner.nextInt();
+
+        final var sum = sumValuesAboveBorder(values, border);
+
+        System.out.println(sum);
+    }
+
+    private static int sumValuesAboveBorder(int[] values, int lowerBorder) {
+        var sum = 0;
+        for (var value : values) {
+            if (value > lowerBorder) {
+                sum += value;
+            }
+        }
+
+        return sum;
     }
 }
