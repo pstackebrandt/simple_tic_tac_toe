af05338308bcd5a3a1ebad3e7e3c74f80848a54b
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2022-06-13 19:04:43 +0200

    Add minimal optimization, tests green task "Maximum element divisible by four" of "for-loop" https://hyperskill.org/learn/step/2189

--- Topics/The for-loop/Maximum element divisible by four/src/Main.java
+++ Topics/The for-loop/Maximum element divisible by four/src/Main.java
@@ -2,23 +2,19 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
-
+        final var scanner = new Scanner(System.in);
         final int numberCount = scanner.nextInt();
-
-        int biggestDividableBy4 = Checker.getBiggestNumberDividableBy4(scanner, numberCount);
+        final int biggestDividableBy4 = Checker.getBiggestNumberDividableBy4(scanner, numberCount);
 
         System.out.println(biggestDividableBy4);
     }
-
-
 }
 
 class Checker {
     public static int getBiggestNumberDividableBy4(Scanner source, int numberCount) {
         int biggestDividableBy4 = 0;
         for (int i = 0; i < numberCount; i++) {
-            int currentNumber = source.nextInt();
+            final int currentNumber = source.nextInt();
             if (isFirstNumberBiggerAndDividableBy4(currentNumber, biggestDividableBy4)) {
                 biggestDividableBy4 = currentNumber;
             }
