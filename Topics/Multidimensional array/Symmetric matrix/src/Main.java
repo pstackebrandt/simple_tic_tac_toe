889fb1b25d1250c5867d9c62f7fe5540b2cba9a5
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2022-11-02 21:42:52 +0100

    lesson finished
    Multidimensional array
    https://hyperskill.org/learn/step/1936

--- Topics/Multidimensional array/Symmetric matrix/src/Main.java
+++ Topics/Multidimensional array/Symmetric matrix/src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+        // get input
+        final var scanner = new Scanner(System.in);
+        final int length = scanner.nextInt();
+        final var matrix = new int[length][length];
+
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        final boolean isSymmetrical = isMatrixSymmetricalToMainDiagonal(length, matrix);
+        System.out.println(getAnswer(isSymmetrical));
+    }
+
+    private static String getAnswer(boolean isSymmetrical) {
+        if (isSymmetrical) return "YES";
+        return "NO";
+    }
+
+    private static boolean isMatrixSymmetricalToMainDiagonal(int length, int[][] matrix) {
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < length; j++) {
+                if (matrix[i][j] != matrix[j][i]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
