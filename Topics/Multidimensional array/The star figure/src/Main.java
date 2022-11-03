63998dcf0eb492c67b6192da6125cadb32b7c674
Peter Stackebrandt <peter.stackebrandt@gmail.com>
2022-11-03 16:52:40 +0100

    The star figure: Add class Figure. add intial code.
    add: create matrix, fill background, print matrix array
    
    The star figure
    https://hyperskill.org/learn/step/1929

--- Topics/Multidimensional array/The star figure/src/Main.java
+++ Topics/Multidimensional array/The star figure/src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import figure.Figure;
+
 class Main {
     public static void main(String[] args) {
-        // put your code here
+
+        final int size = new Scanner(System.in).nextInt();
+        var figure = new Figure(size);
+        figure.show();
     }
+
+
 }
